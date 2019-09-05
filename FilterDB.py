35a8ee75f5b375ad3252011e3df656a0bb6f3ab9
Morten Mathiasen <morten@tranebaerkaeret.dk>
2019-09-05 18:28:31 +0200

    Avoid huge memory load on large amount ads to be done

--- FilterDB.py
+++ FilterDB.py
@@ -21,59 +21,67 @@ class FilterDB:
             fetchCursor = cnx.cursor(buffered=True)
 
             # Enforce UTF-8 for the connection.
+            print('Now connecting to....: %s' % cnx.server_host, flush=True)
             fetchCursor.execute('SET NAMES utf8mb4')
             fetchCursor.execute("SET CHARACTER SET utf8mb4")
             fetchCursor.execute("SET character_set_connection=utf8mb4")
+            print('Successfully connected to: %s' % cnx.server_host, flush=True)
 
-            print('Now connecting to....: %s' % cnx.server_host, flush=True)
             #fetchCursor.execute('SELECT _id,body FROM annonce where _id = 762147')
 
-            fetchCursor.execute('SELECT _id,body FROM annonce where searchable_body IS NULL OR lastSearchableBody IS NULL OR lastUpdated < lastSearchableBody')
+            limit = 1000
+            offset = 0
+            while True:
+                fetchCursor.execute('SELECT _id,body FROM annonce where searchable_body IS NULL OR lastSearchableBody IS NULL OR lastUpdated < lastSearchableBody LIMIT {0},{1}'.format(offset, limit))
+                offset = offset + limit
+                fetchCount = 0
 
-            print('Successfully connected to: %s' % cnx.server_host, flush=True)
-            startTimer = time.time()
-            print('{%s} Filter started' % datetime.datetime.now().strftime('%H:%M:%S'), flush=True)
-            for _id, body in fetchCursor:
-                print("Inserting searchable_body for id: %s" % _id, flush=True)
-                #print("fetchCursor: %s" % fetchCursor, flush=True)
-                convertToSting = str(body.encode().decode())
-                # print(convertToSting, flush=True)
-                removespaces = re.compile(r'\s+')
-                advertBody = re.sub(removespaces, ' ', convertToSting)
-                soup = BeautifulSoup(advertBody, 'html.parser')
-                searchable_body = self.walker(soup)
-                #print("searchable_body:\n%s", searchable_body)
-                #cvr_reg = re.compile("(cvr.{0,10})(?!21367087)(([0-9] ?){8})")
-                cvr_reg = re.compile("(?i)((cvr|vat).{0,10})(([0-9] ?){8})")
-                cvr_list = cvr_reg.findall(searchable_body)
-                #print("cvr_list:\n%s" % cvr_list, flush=True)
-                cvr = None
-                if cvr_list:
-                    cvr = cvr_list[0][2].replace(" ", "")
-                if cvr is not None and cvr != "21367087":
-                        # API CALL
-                    url = "http://distribution.virk.dk/cvr-permanent/_search"
-                    data = {"query": {"term": {"Vrvirksomhed.cvrNummer": cvr}}}
-                    data = json.dumps(data)
-                    headers = {'Content-type': 'application/json; charset=utf-8', 'user-agent': 'EAAA'}
+                startTimer = time.time()
+                print('{%s} Filter started' % datetime.datetime.now().strftime('%H:%M:%S'), flush=True)
+                for _id, body in fetchCursor:
+                    fetchCount = fetchCount + 1
+                    print("Inserting searchable_body for id: %s" % _id, flush=True)
+                    #print("fetchCursor: %s" % fetchCursor, flush=True)
+                    convertToSting = str(body.encode().decode())
+                    # print(convertToSting, flush=True)
+                    removespaces = re.compile(r'\s+')
+                    advertBody = re.sub(removespaces, ' ', convertToSting)
+                    soup = BeautifulSoup(advertBody, 'html.parser')
+                    searchable_body = self.walker(soup)
+                    #print("searchable_body:\n%s", searchable_body)
+                    #cvr_reg = re.compile("(cvr.{0,10})(?!21367087)(([0-9] ?){8})")
+                    cvr_reg = re.compile("(?i)((cvr|vat).{0,10})(([0-9] ?){8})")
+                    cvr_list = cvr_reg.findall(searchable_body)
+                    #print("cvr_list:\n%s" % cvr_list, flush=True)
+                    cvr = None
+                    if cvr_list:
+                        cvr = cvr_list[0][2].replace(" ", "")
+                        if cvr is not None and cvr != "21367087":
+                            # API CALL
+                            url = "http://distribution.virk.dk/cvr-permanent/_search"
+                            data = {"query": {"term": {"Vrvirksomhed.cvrNummer": cvr}}}
+                            data = json.dumps(data)
+                            headers = {'Content-type': 'application/json; charset=utf-8', 'user-agent': 'EAAA'}
 
-                    response = requests.post(url=url, auth=(os.environ['API_USERNAME'], os.environ['API_PASSWORD']), data=data, headers=headers)
+                            response = requests.post(url=url, auth=(os.environ['API_USERNAME'], os.environ['API_PASSWORD']), data=data, headers=headers)
 
-                    if response.status_code is 200:
-                        print("Inserting cvr %s" % cvr)
-                        self.insertGenericToDB(key="cvr", value=cvr, condition=_id)
+                            if response.status_code is 200:
+                                print("Inserting cvr %s" % cvr)
+                                self.insertGenericToDB(key="cvr", value=cvr, condition=_id)
 
-                        company = response.text
-                        print("Inserting company json")
-                        #print(company)
-                        self.insertGenericToDB(key="json", value=company, condition=_id)
+                                company = response.text
+                                print("Inserting company json")
+                                #print(company)
+                                self.insertGenericToDB(key="json", value=company, condition=_id)
+                                self.insertToDB(searchable_body=searchable_body, condition=_id)
+                    else:
                         self.insertToDB(searchable_body=searchable_body, condition=_id)
-                else:
-                    self.insertToDB(searchable_body=searchable_body, condition=_id)
-                # print(searchable_body , flush=True)
-            elapsed = time.time() - startTimer
-            duration = time.strftime('%H:%M:%S', time.gmtime(elapsed))
-            print('Took: %s' % duration, flush=True)
+                        # print(searchable_body , flush=True)
+                elapsed = time.time() - startTimer
+                duration = time.strftime('%H:%M:%S', time.gmtime(elapsed))
+                print('Took: %s' % duration, flush=True)
+                if fetchCount < limit:
+                    break;
         except Error as e:
             print(e.args, flush=True)
         finally:
